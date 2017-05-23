162b9d12bed6a970fe401951bb3a30fd2824d16f
Wramberg <jesper.wramberg@gmail.com>
2017-05-23 23:04:31 +0200

    Lots of tweaks and cleanup

--- setup.sh
+++ setup.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
 inst="sudo apt install -y"
+pip="sudo pip install"
+sudo apt update
 
 # Install i3
 $inst i3
@@ -14,7 +16,7 @@ $inst i3lock
 
 # Filemanagers
 $inst ranger
-#$inst thunar
+$inst thunar
 
 # Automount
 $inst udiskie
@@ -29,7 +31,42 @@ $inst lxappearance
 $inst volumeicon-alsa
 
 # PDF reader
-$inst evince 
+$inst evince
+
+# pip
+$inst python-pip
+$pip --upgrade pip
+
+# py3status
+$pip py3status
+
+# Deps for py3status clock module
+$pip pytz
+$pip tzlocal
+
+# Deps for custom py3status modules
+$pip psutil
+
+# Wallpaper
+$inst feh
+feh --bg-scale b6xnLhT.jpg
+
+# Install rxvt terminal
+#sudo software-properties-gtk -e universe
+#sudo apt update
+#$inst rxvt
+
+# Install mate terminal
+$inst mate-terminal
+
+# Install arandr
+$inst arandr
+
+# Remove nautilus
+sudo apt remove -y nautilus
+
+# Remove dunst
+# sudo apt remove -y dunst
 
 # Set up i3 config link
 mkdir -p $HOME/.config/i3/
@@ -38,4 +75,6 @@ ln -s $HOME/wramberg_linux_setup/i3_config $HOME/.config/i3/config
 echo "Done - remember:"
 echo "lxappearance"
 echo "ranger bashrc hack"
-echo "volume controls in tray"
+echo "volume controls keybindings in tray"
+echo "terminal colors"
+echo "terminal menu"
