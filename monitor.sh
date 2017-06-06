99910f3b513db54bdd445156527a58e0399ca4cb
Jesper Wramberg <jesper.wramberg@gmail.com>
2017-06-06 08:43:39 +0200

    Add monitor shortucts, battery level and citrix floating

---
+++ monitor.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+fullhd1="--mode 1920x1200"
+laptop="--output eDP-1"
+
+case "$1" in
+    "docked") xrandr --output DP-2-1 $fullhd1 --output DP-2-2 $fullhd1 --right-of DP-2-1 $laptop --off ;;
+    *) xrandr --auto
+esac
+
