fc61e7c26ec6de624c3449d4981d77ded5289d19
Jesper Wramberg <jesper.wramberg@gmail.com>
2017-06-30 11:33:30 +0200

    Move workspaces around and fix monitor script

--- monitor.sh
+++ monitor.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 
-work_screen_res="--mode 2560x1440"
 laptop="--output eDP-1"
 dp1="--output DP-2-1"
 dp2="--output DP-2-2"
 
 case "$1" in
-    "docked") xrandr $dp1 $work_screen_res $dp2 $work_screen_res --right-of $dp1 $laptop --off ;;
+    "docked") xrandr $dp1 --auto $dp2 --auto --right-of DP-2-1 $laptop --off ;;
     *) xrandr --auto
 esac
 
