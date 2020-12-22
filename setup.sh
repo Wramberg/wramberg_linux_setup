04fdd69bab957c79ced517d2dc171d47e8cf80ba
Your Name <you@example.com>
2020-12-22 21:06:25 +0100

    Rough draft for 20.04

--- setup.sh
+++ setup.sh
@@ -2,13 +2,26 @@
 
 inst="sudo apt install -y"
 pip="sudo pip install"
+
 sudo apt update
 
-# Install i3
-$inst i3
+# Install i3 gaps deps and i3 gaps itself
+$inst libxcb1-dev libxcb-keysyms1-dev libpango1.0-dev \
+	libxcb-util0-dev libxcb-icccm4-dev libyajl-dev \
+	libstartup-notification0-dev libxcb-randr0-dev \
+	libev-dev libxcb-cursor-dev libxcb-xinerama0-dev \
+	libxcb-xkb-dev libxkbcommon-dev libxkbcommon-x11-dev \
+	autoconf libxcb-xrm0 libxcb-xrm-dev automake libxcb-shape0-dev \
+	meson
 
-# Install network applet
-#$inst nm-applet
+git clone https://www.github.com/Airblader/i3 i3-gaps
+cd i3-gaps
+
+# compile & install
+mkdir -p build && cd build
+sudo meson ..
+sudo ninja
+sudo meson install
 
 # Autolock and lock
 $inst xautolock
@@ -19,7 +32,7 @@ $inst ranger
 $inst thunar
 
 # Automount
-$inst udiskie
+#$inst udiskie
 
 # Image viewer
 $inst sxiv
@@ -28,33 +41,22 @@ $inst sxiv
 $inst lxappearance
 
 # Soundmanager
-$inst volumeicon-alsa
+#$inst volumeicon-alsa
 
 # PDF reader
 $inst evince
 
+# Install polybar
+$inst compton
+$inst polybar
+
 # pip
-$inst python-pip
-$pip --upgrade pip
-
-# py3status
-$pip py3status
-
-# Deps for py3status clock module
-$pip pytz
-$pip tzlocal
-
-# Deps for custom py3status modules
-$pip psutil
+$inst python3-pip
+$pip --upgrade pip3
 
 # Wallpaper
 $inst feh
-feh --bg-scale b6xnLhT.jpg
-
-# Install rxvt terminal
-#sudo software-properties-gtk -e universe
-#sudo apt update
-#$inst rxvt
+feh --bg-fill wallpapers/1449203821767.jpg
 
 # Install mate terminal
 $inst mate-terminal
@@ -70,7 +72,9 @@ $inst rofi
 
 # Remove nautilus - install thunar as backup filemanager
 $inst thunar
-sudo apt remove -y nautilus
+
+# For sound control
+#$inst pavucontrol
 
 # Remove dunst
 # sudo apt remove -y dunst
@@ -78,6 +82,10 @@ sudo apt remove -y nautilus
 # Set up i3 config link
 mkdir -p $HOME/.config/i3/
 ln -s $HOME/wramberg_linux_setup/i3_config $HOME/.config/i3/config
+
+# Setup polybar config link
+mkdir -p $HOME/.config/polybar/
+ln -s $HOME/wramberg_linux_setup/polybar_config $HOME/.config/polybar/config
 
 # When quitting ranger cd into dir it was standing in
 cmd="alias ranger='ranger --choosedir=/tmp/.rangerdir;cd \$(cat /tmp/.rangerdir)'"
@@ -92,16 +100,16 @@ if [ $result -eq 1 ]; then
 fi
 
 # Set editor to sublime text in bashrc
-editor="export EDITOR='subl -b'"
-title="#default-editor"
+#editor="export EDITOR='subl -b'"
+#title="#default-editor"
 
-grep "$title" $HOME/.bashrc > /dev/null
-result=$?
-if [ $result -eq 1 ]; then
-    echo "" >> $HOME/.bashrc
-    echo $title >> $HOME/.bashrc
-    echo $editor >> $HOME/.bashrc
-fi
+#grep "$title" $HOME/.bashrc > /dev/null
+#result=$?
+#if [ $result -eq 1 ]; then
+#    echo "" >> $HOME/.bashrc
+#    echo $title >> $HOME/.bashrc
+#    echo $editor >> $HOME/.bashrc
+#fi
 
 # Customize notify a bit - https://askubuntu.com/questions/128474/how-to-customize-on-screen-notifications
 # sudo add-apt-repository ppa:leolik/leolik
@@ -113,11 +121,31 @@ fi
 # sudo apt-get update
 # sudo apt-get install notifyosdconfig
 
+# Install fonts
+$inst fonts-material-design-icons-iconfont
+#cd /tmp && git clone https://github.com/stark/siji && cd siji && ./install.sh -d $HOME/.fonts
+
+# Add usb dev script to polybar
+mkdir $HOME/polybar-scripts/
+ln -s $HOME/wramberg_linux_setup/rofi-usb-mount.sh $HOME/polybar-scripts/rofi-usb-mount.sh
+
+# Add rofi theme
+mkdir -p $HOME/.config/rofi/
+ln -f -s $HOME/wramberg_linux_setup/rofi.rasi $HOME/.config/rofi.rasi
+ln -f -s $HOME/wramberg_linux_setup/rofi_config $HOME/.config/rofi/config
+
+# Fix nm applet icon
+mkdir -p $HOME/.icons/Humanity/devices/16/
+mkdir -p $HOME/.icons/Humanity/status/16/
+ln -f -s /usr/share/icons/ubuntu-mono-dark/status/24/nm-device-wired.svg $HOME/.icons/Humanity/devices/16/nm-device-wired.svg
+ln -f -s /usr/share/icons/ubuntu-mono-dark/status/24/nm-no-connection.svg $HOME/.icons/Humanity/status/16/nm-no-connection.svg
+
 
 echo "Done - remember:"
 echo "lxappearance - widget clearlook"
 echo "lxappearance - icons"
 echo "volume controls keybindings in tray"
 echo "volume controls icon"
-echo "terminal - colors"
+echo "terminal - colors and transparancy"
 echo "terminal - hide menu on startup"
+echo "terminal - copy on selection"
